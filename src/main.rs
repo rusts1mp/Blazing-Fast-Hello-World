bc18edae03685a29251356a140a77f3c496e1429
Rustin Ferris <rustinferris@gmail.com>
2022-09-23 10:44:27 -0500

    make it blazingly fast 🚀

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::io::Write;
+// i used rayon so it's blazing fast 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
+use rayon::prelude::*;
+
+static INDEX: AtomicUsize = AtomicUsize::new(0);
+static MSG: &[u8] = b"Hello, world!";
+
 fn main() {
-    let message = "Hello, world!";
-    println!("{message}");
+    (0..MSG.len()).into_par_iter().for_each(|_| {
+        let i = INDEX.fetch_add(1, Ordering::Relaxed);
+        let c = &MSG[i..][..1];
+        std::io::stdout().lock().write_all(c).unwrap();
+    })
 }
